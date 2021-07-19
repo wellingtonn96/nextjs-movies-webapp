21b3895951c2f17a903ba0409b026ac375c6eac2
Wellington Santos <weltossousa@gmail.com>
2021-07-19 02:30:07 -0300

    chaging enviroment variables to deploy

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,5 +1,7 @@
+const dev = process.env.NODE_ENV !== 'production';
+
 export const args = {
-  host: process.env.VERCEL_URL,
+  host: dev ? 'http://localhost:3000' : process.env.NODE_ENV,
   port: process.env.PORT,
   api_key: process.env.API_KEY,
   base_url: process.env.BASE_URL,
